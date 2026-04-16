7fc7385fa3c8d0b88d21acb9c92881fde7832e1a
gowthams-cdw <gowtham.s@cdw.com>
2026-04-16 08:37:37 +0530

    Add generate random colors each time

--- color_randomizer_http/src/index.js
+++ color_randomizer_http/src/index.js
@@ -1,7 +1,8 @@
 // imports
+
+import * as dotenv from "dotenv";
 import fs from "fs";
 import http from "http";
-import * as dotenv from "dotenv";
 
 // setup environment variables
 dotenv.config();
@@ -12,40 +13,41 @@ const srcFilePath = "src/data/color_palette.json";
 const inputFile = fs.readFileSync(srcFilePath, "utf-8");
 const jsonInputFileFormat = JSON.parse(inputFile);
 
-// picking random colors
-const randomColorCount = 5;
-const randomColors = [];
-const colorSet = new Set();
-for (let i = 0; i < randomColorCount; i++) {
-	// to get unique random color
-	while (true) {
-		const randomIndex = Math.floor(Math.random() * jsonInputFileFormat.length);
-		if (colorSet.has(randomIndex)) {
-			continue;
-		}
-
-		// add to new hashset
-		randomColors.push(jsonInputFileFormat[randomIndex]);
-		colorSet.add(randomIndex);
-		break;
+// create a function to generate random colors each time
+const generateRandomColors = (randomColorCount) => {
+	// if randomColorCount is higher than avail colors, risk of infinite loop
+	if (randomColorCount > jsonInputFileFormat.length) {
+		throw new Error(
+			"Random color count exceeds available colors in the palette.",
+		);
 	}
-}
+
+	const randomColors = [];
+	for (let i = 0; i < randomColorCount; i++) {
+		const randomIndex = Math.floor(Math.random() * jsonInputFileFormat.length);
+
+		randomColors.push(jsonInputFileFormat[randomIndex]);
+		jsonInputFileFormat.splice(randomIndex, 1);
+	}
+
+	return randomColors;
+};
 
 // create server
 const server = http.createServer((req, res) => {
 	if (req.url === "/") {
 		res.statusCode = 200;
 		res.setHeader("Content-Type", "application/json");
-		res.end(JSON.stringify(randomColors));
+		res.end(JSON.stringify(generateRandomColors(5)));
 	} else {
 		res.statusCode = 404;
 		res.setHeader("Content-Type", "application/json");
 		res.end(JSON.stringify({ error: "Not Found" }));
 	}
-})
+});
 
 // listen server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
 	console.log(`Server running at http://localhost:${PORT}/`);
-})
+});
